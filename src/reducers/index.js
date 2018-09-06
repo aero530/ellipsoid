50288c43a5745b09161b64907774f6479043a337
aero530 <aero530@gmail.com>
2018-09-06 16:22:09 -0500

    syntax updates

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form'
+import { reducer as formReducer } from 'redux-form';
 import geometryReducer from './geometry';
 import fileReducer from './file';
 
